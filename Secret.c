81e47f4ad6ad4b56b6b27892cb15dc4e385978fd
seed <seed@comp435-noahbrow.cs.unc.edu>
2026-09-10 16:00:03 -0400

    pushes starter code

---
+++ Secret.c
@@ -0,0 +1,2 @@
+#include "Secret.h"
+char *secret = "Some Secret Value"; // you can change this to try new values--the autograder will have its own Secret.c
---
+++ Secret.h
@@ -0,0 +1,4 @@
+#ifndef SECRET_H
+#define SECRET_H
+extern char *secret; // define an external secret
+#endif
